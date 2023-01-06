8c9c8dd1a0eefc4c4fe79e2767981a3c21c13657
akshansh <chauhanakshansh57@gmail.com>
2023-01-06 19:47:14 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+const port = process.env.PORT || 1000;
+let app = express();
+let server = http.createServer(app);
+let io = new Server(server);
+
+app.use(express.static("material"));
+
+app.get('/', (q, r) => {
+    r.sendFile(__dirname + "/home/index.html");
+})
+
+app.get('/peer1', (q, r) => {
+    r.sendFile(__dirname + "/home/peer1.html")
+})
+
+app.get('/peer2', (q, r) => {
+    r.sendFile(__dirname + "/home/peer2.html")
+})
+
+app.get('/socket.io', (q, r) => {
+    r.sendFile(__dirname + "/node_modules/socket.io/client-dist/socket.io.min.js")
+})
+var count_user = 0;
+
+io.sockets.on("connect", (o) => {
+
+    count_user++;
+
+    o.on("disconnect", () => {
+        count_user--
+        console.log();
+    })
+
+    console.log("connected", count_user, o.id)
+
+    o.on("p1off", (p1off) => {
+        console.log(p1off)
+        o.broadcast.emit("p1offer", p1off)
+    })
+
+    o.on("p2ans", (p2ans) => {
+        console.log(p2ans)
+        o.broadcast.emit("p2answer", p2ans)
+    })
+
+    o.on("reload", () => {
+        o.broadcast.emit("reload", {})
+    })
+
+})
+
+server.listen(port, () => {
+    console.log('listning on port ' + port);
+})
