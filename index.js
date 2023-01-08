dc54f2cbd19daf2f6c7bbab65f5b412e90a07f40
akshansh <chauhanakshansh57@gmail.com>
2023-01-08 18:59:42 +0530

    uopppeii commit

--- index.js
+++ index.js
@@ -35,18 +35,26 @@ var faq = {
         "how many people connected at one time",
         "location area coordinates",
         "mobile number",
-        "time"
+        "what is this",
+        "webrtc",
+        "time",
+        "collage itg itgopeshwar student of",
+        "help, how to start",
     ],
     a: [
         "hi, how can i help you?",
-        "Akshansh Chauhan Develop Me",
-        "I am Created by Akshansh Chauhan",
-        "I am build by Akshansh Chauhan",
-        "This site is for Communication, Through Video and Audio",
-        "Mainly Two, as peer1 and peer2. But you can make Several Peer1 and Peer2 at a same time",
-        "Uttarakhand, India",
-        "+91 ******5377, :)",
-        gtTime()
+        "<b>Akshansh Chauhan</b> Develop Me",
+        "I am Created by <b>Akshansh Chauhan</b>",
+        "I am build by <b>Akshansh Chauhan</b>",
+        "This site is for <b>Communication</b>, Through <b>Video</b> and <b>Audio</b>",
+        "Mainly <b>Two</b>, as <mark>peer1</mark> and <mark>peer2</mark>. But you can make Several Peer1 and Peer2 at a same time",
+        "Uttarakhand, <b>India</b>",
+        "<b>+91</b> ******5377, :)",
+        "This is <mark>real time communication</mark> web application",
+        "<img src='https://res.cloudinary.com/practicaldev/image/fetch/s--GQZMu9WG--/c_imagga_scale,f_auto,fl_progressive,h_500,q_66,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/9tsx7bf26ejjb1wcj2ve.gif' width='200px' alt='webrtc.img.image'><br><br><h3>Introduction to WebRTC</h3><br><aside>WebRTC stands for Web Real-Time Communication. It is an open source and free project that used to provide real-time communication to mobile applications and web browsers with the help of API’s(Application Programming Interface).</aside><a href='https://www.geeksforgeeks.org/introduction-to-webrtc/'>more.</a>",
+        gtTime(),
+        "<img src='https://itgopeshwar.ac.in/wp-content/uploads/2020/07/cropped-ITG-LOGO-2-1-446x79.png' width='200px' style='background-color: white;'><br><br><h3>Institute of Technology, Gopeshwar Founded in 2013</h3><br><p>Institute of Technology, Gopeshwar is one of the Four constituent colleges of Uttarakhand Technical University that was established in October 2013 with the approval of Uttarakhand Government. IT Gopeshwar is approved by AICTE,Ministry of HRD, New Delhi. IT Gopeshwar,is an Institute which aims to promote technological advancement in the state of uttarakhand. It provides such a conductive environment to his students in which they become intellectually capable, innovative and entrepreneurial professionals. IT Gopeshwar also provides an educational training that promotes and emphasizes social awareness as well as development of professional skills among the students. Here the knowledge of the students is broadened, deepened and enriched as the institute provide such solutions that benefit humanity and the natural environment.</p><a href='https://itgopeshwar.ac.in'>More.</a>",
+        "<h2>Watch this Video :</h2><br><video src='tutorial.mp4' width='200px' controls maximize autoplay></video>"
     ]
 }
 
@@ -67,13 +75,27 @@ app.get('/', (q, r) => {
     r.sendFile(__dirname + "/home/index.html");
 })
 
-app.post('/faq', (q, r) => {
-    if (faq.q.includes(q.body.quest)) {
-        r.send(faq.a[faq.q.indexOf(q.body.quest)])
+var ind = 0;
+var cond = false
+app.post('/faq', async(q, r, err) => {
+    faq.q.forEach((e, i) => {
+        if (e.includes(q.body.quest)) {
+            ind = i
+            cond = true
+        }
+    })
+    if (cond == true) {
+        r.send(faq.a[ind])
+        ind = 0
     } else {
-        r.send("I don's Understand You :(")
+        r.send("sorry, i can't get!")
         fs.appendFileSync("faq.txt", "question [" + gtTime() + "] : " + q.body.quest + "\n", "utf-8")
+        ind = 0
     }
+})
+
+app.get('/faq/q/', (q, r) => {
+    r.sendFile(__dirname + "/faq.txt")
 })
 
 app.get('/jquery', (q, r) => {
